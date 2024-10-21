1b5af212fa320b8afd0c6e6b1d02221f101bd1dd
aibeknet <aaskarov0690.kg@gmail.com>
2024-10-21 17:11:50 +0600

    The About component has been added.

---
+++ src/containers/About/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>About Us</h1>
+      <p>Information about me.</p>
+    </div>
+  );
+};
+
+export default About;
