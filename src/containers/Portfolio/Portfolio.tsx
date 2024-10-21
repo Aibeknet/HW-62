7db10da98014d4bf2e7935028d5804bc6bb24878
aibeknet <aaskarov0690.kg@gmail.com>
2024-10-21 17:33:19 +0600

    The Portfolio component has been added.

---
+++ src/containers/Portfolio/Portfolio.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Portfolio: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>My Portfolio</h1>
+      <p>Projects and works I've done.</p>
+    </div>
+  );
+};
+
+export default Portfolio;
