4c1df4192f8b2b06e552f92a09a99ebe00b9c61b
aibeknet <aaskarov0690.kg@gmail.com>
2024-10-21 17:13:39 +0600

    The Contacts component has been added.

---
+++ src/containers/Contacts/Contacts.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contacts: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>Contact Us</h1>
+      <p>Contact information.</p>
+    </div>
+  );
+};
+
+export default Contacts;
